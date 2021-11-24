71845ce97c789f7a2c2cf1c8c0b3af7397f9bf5a
guenga1968 <gustavogallesio@hotmail.com>
2021-11-24 18:13:52 -0300

    ejwmplo

--- prueba.py
+++ prueba.py
@@ -1,29 +1,7 @@
-# CLASES EJERCICIO 2
-class Vectores:
-    def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __add__(self, other):
-        return Vectores(self.x + other.x, self.y + other.y, self.z + other.z)
-
-    def __sub__(self, other):
-        return Vectores(self.x - other.x, self.y - other.y, self.z - other.z)
-
-    def __mul__(self, other):
-        return Vectores(self.x * other.x, self.y * other.y, self.z * other.z)
-
-    def __truediv__(self, other):
-        return Vectores(self.x / other.x, self.y / other.y, self.z / other.z)
-
-    def __str__(self):
-        return "({}, {}, {})".format(self.x, self.y, self.z)
-   
-vector1 = Vectores(2, 3, 8)
-vector2 = Vectores(4, 5, 6)
-print(vector1 + vector2)
-print(vector1 - vector2)
-print(vector1 * vector2)
-print(vector1 / vector2)
-# --------------------------------------------------
+def palabra(palabra):
+   palabra = palabra.slice(0, -1)
+   for i in palabra:
+        letra= palabra.count(i)
+        print(i,letra)
+       
+palabra(input("Ingrese una palabra: "))
